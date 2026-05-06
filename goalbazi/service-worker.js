18ac719cef81db835256b00fde877528d822f0b4
labibhaider4-hash <labib.haider4@gmail.com>
2026-05-06 12:38:02 +0530

    Add files via upload

--- goalbazi/service-worker.js
+++ goalbazi/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "goalbazi-pwa-v31";
+const CACHE_NAME = "goalbazi-pwa-v32";
 
 // Files that make the installed PWA open quickly and still show a useful page offline.
 const APP_SHELL = [
